b959fadae29a40c580cbbd8757150e5dc57bb221
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-01 21:00:05 +0400

    initial header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Image galelry</h1>
+    <div>
+      <Header title="Images Gallery" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
