538e966082dd01848aac9cd35ffad3a7e049c876
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-04 01:55:24 +0400

    enabled pyling and black code formatter

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
@@ -12,7 +11,9 @@ UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
 
 app = Flask(__name__)
 CORS(app)
@@ -23,15 +24,12 @@ app.config["DEBUG"] = DEBUG
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-        "Accept-version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
