df3a43ddafa6f6c69dbb3eff52fbd603c5149f06
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-04 13:34:28 +0400

    dockerfile for the frontend service

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # docker run -p 5050:5050 images-gallery-api
+# docker run -p 3000:3000 images-gallery-frontend
+docker build . -t images-gallery-frontend
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [svgr(), react()],
 });
