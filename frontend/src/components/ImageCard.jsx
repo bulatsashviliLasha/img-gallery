38ef141ab90aae6828caaff46d76afa00fc51d5a
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-07 00:14:41 +0400

    saving images in mongo

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-const ImageCard = ({ image, deleteImage }) => {
+const ImageCard = ({ image, deleteImage, saveImage }) => {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img
@@ -10,11 +10,12 @@ const ImageCard = ({ image, deleteImage }) => {
         src={image.urls.small}
       />
       <Card.Body>
-        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Title>{image?.title?.toUpperCase()}</Card.Title>
         <Card.Text>{image?.description || image?.alt_description}</Card.Text>
         <Button onClick={() => deleteImage(image.id)} variant="danger">
           Delete
-        </Button>
+        </Button>{' '}
+          {!image.saved && <Button onClick={() => saveImage(image.id)} variant="primary">Save</Button>}
       </Card.Body>
     </Card>
   );
